677f92a6b08b53fc766fc7d82bb719e3d68e3c91
Talha Koc <talha.koc@duke.edu>
2017-02-25 21:37:32 -0500

    added handler to UIMain

--- src/gui/UIMain.java
+++ src/gui/UIMain.java
@@ -1,8 +1,14 @@
 package gui;
 
+import controller.ControlHandler;
+
 public class UIMain {
+	ControlHandler handler;
 	
-	
+	public UIMain(ControlHandler handler){
+		super();
+		this.handler = handler;
+	}
 	public void displayErrorWithMessage(String error){
 		
 	}
