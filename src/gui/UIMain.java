f1b2d82a86b106c5a57e141f8258038712336ab1
Talha Koc <talha.koc@duke.edu>
2017-02-25 21:49:40 -0500

    updated UIMain and added its API

--- src/gui/UIMain.java
+++ src/gui/UIMain.java
@@ -1,13 +1,21 @@
 package gui;
 
+import java.util.List;
+
 import controller.ControlHandler;
 
-public class UIMain {
+public class UIMain implements UIMainAPI {
+	
 	ControlHandler handler;
+	UITerminalView terminal;
+	UITurtleDisplayView display;
+	UIVariablesView varBox;
+	UIFunctionsTable vocabBox;
 	
 	public UIMain(ControlHandler handler){
 		super();
 		this.handler = handler;
+		setupViews();
 	}
 	public void displayErrorWithMessage(String error){
 		
@@ -15,4 +23,17 @@ public class UIMain {
 	public void sendTextOutput(String output){
 		
 	}
+	public void updateVocabBox(List<String> words) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void updateVarBox(List<String> variables) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void setupViews(){
+		
+	}
+
 }
