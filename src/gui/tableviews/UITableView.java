46bda3ca9568a6538b2ae47c823688e994ea92f1
Talha Koc <talha.koc@duke.edu>
2017-03-02 03:02:01 -0500

    minor changes for basic

--- src/gui/tableviews/UITableView.java
+++ src/gui/tableviews/UITableView.java
@@ -45,7 +45,7 @@ public class UITableView extends Pane {
 		setupTitle(title);
 
 		_flowPane = new FlowPane(Orientation.VERTICAL);
-		_flowPane.setPadding(new Insets(40, 12, 10, 12));
+		_flowPane.setPadding(new Insets(48, 12, 10, 12));
 		_flowPane.setVgap(20);
 
 		
@@ -65,7 +65,8 @@ public class UITableView extends Pane {
 	private void setupTitle(String t){
 		Label title = GUITools.plainLabelBoldHelvetica(t, 20, MyColors.INDIGO);
 		title.setPrefWidth(_bounds.getWidth());
-		title.setPrefHeight(48);
+		title.setPrefHeight(40);
+		title.setAlignment(Pos.CENTER);
 		this.getChildren().add(title);
 	}
 	protected void addText(String s){
