33434d1cdbebe4d90037986b3abddb022bcd5f94
Gideon Pfeffer <gap16@duke.edu>
2017-03-11 00:41:33 -0500

    refactored the factory

--- src/parser/reflection/ClassGenerator.java
+++ src/parser/reflection/ClassGenerator.java
@@ -19,42 +19,29 @@ public class ClassGenerator {
 	public TreeNode generate(String className, List<TreeNode> list, State t) {
 		Class<?> clazz = getClass(className);
 		Constructor<?> ctor = makeConstructor(clazz, List.class, TCH.isTurtleCommand(className));
-		return makeCommandClass(clazz, list, t, TCH.isTurtleCommand(className), ctor);
+		return makeClass(clazz, list, t, TCH.isTurtleCommand(className), ctor);
 	}
 	
 	public TreeNode generate(String className, Double num, State t) {
 		Class<?> clazz = getClass(className);
 		Constructor<?> ctor = makeConstructor(clazz, double.class, TCH.isTurtleCommand(className));
-		return makeConstantClass(clazz, num, t, TCH.isTurtleCommand(className), ctor);
+		return makeClass(clazz, num, t, TCH.isTurtleCommand(className), ctor);
 	}
 	
-	private TreeNode makeConstantClass(Class<?> clazz, double num, State t,
+	private TreeNode makeClass(Class<?> clazz, Object obj, State t,
 			boolean isTurtleCommand, Constructor<?> ctor){
 		try{
 			if(isTurtleCommand){
-				return (TreeNode) clazz.cast(ctor.newInstance(num, t));
+				return (TreeNode) clazz.cast(ctor.newInstance(obj, t));
 			}
 			else{
-				return (TreeNode) clazz.cast(ctor.newInstance(num));
+				return (TreeNode) clazz.cast(ctor.newInstance(obj));
 			}
 		} catch (Exception e){
 			throw new SLogoException("invalid syntax: " + clazz.getClass());
 		}
 	}
-	
-	private TreeNode makeCommandClass(Class<?> clazz, List<TreeNode> list, State t,
-			boolean isTurtleCommand, Constructor<?> ctor){
-		try{
-			if(isTurtleCommand){
-				return (TreeNode) clazz.cast(ctor.newInstance(list, t));
-			}
-			else{
-				return (TreeNode) clazz.cast(ctor.newInstance(list));
-			}
-		} catch (Exception e){
-			throw new SLogoException("invalid syntax: " + clazz.getClass());
-		}
-	}
+
 	
 	protected Class<?> getClass(String className){
 		Class<?> clazz;
